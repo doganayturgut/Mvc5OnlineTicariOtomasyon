fa753e1164c3218f710013a213249c90809656bf
DOĞANAY TURGUT <doganayturgut34@gmail.com>
2022-09-23 13:24:31 +0300

    Yeni Düzenlemeler

--- Mvc5OnlineTicariOtomasyon/Models/Siniflar/Personel.cs
+++ Mvc5OnlineTicariOtomasyon/Models/Siniflar/Personel.cs
@@ -24,6 +24,7 @@ namespace Mvc5OnlineTicariOtomasyon.Models.Siniflar
         [StringLength(250)]
         public string PersonelGorsel { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
-        public Departman Departman { get; set; }
+        public int Departmanid { get; set; }
+        public virtual Departman Departman { get; set; }
     }
 }
